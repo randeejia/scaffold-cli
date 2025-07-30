99c7218fe2156cd618b79dce598d7b4bb415cb62
贾庆超 <jiaqc5@chinaunicom.cnm>
2025-07-30 15:39:39 +0800

    fix: 格式化后又回到了原来的单行格式问题

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,14 +4,18 @@ module.exports = {
     ecmaVersion: 2020,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
-  extends: ['eslint:recommended', 'prettier', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
     '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/no-explicit-any': 'warn',
     'prefer-const': 'error',
     'no-var': 'error',
+    // 关闭与Prettier冲突的规则
+    quotes: 'off',
+    semi: 'off',
+    'comma-dangle': 'off',
+    'max-len': 'off',
+    indent: 'off',
   },
   env: {
     node: true,
